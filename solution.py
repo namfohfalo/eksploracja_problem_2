6ba26046e1d2a598c0b1d3bc5031c367f705dc35
dudenzz <kubadt@gmail.com>
2025-04-15 01:13:27 +0200

    first commit

---
+++ solution.py
@@ -0,0 +1,5 @@
+def what_types(total:float, atk:float, dfs:float, spatk : float, spdfs : float, spd : float, gen : float, is_leg: bool):
+    return 'type1', 'type2'
+
+def is_legendary(type1: str, type2: str, total:float, atk:float, dfs:float, spatk : float, spdfs : float, spd : float, gen : float):
+    return False
